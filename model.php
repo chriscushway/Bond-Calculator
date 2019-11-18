722f7c8f5ed75a5be9b4eedc9966a4eddb7b97ee
chris <chriscushway@gmail.com>
2019-11-18 21:40:57 +0200

    succesffuly able to insert calculations into database

--- model.php
+++ model.php
@@ -3,7 +3,18 @@
 
     if($_GET['action']=="save"){
         print_r($_POST);
-       /* $query = "INSERT INTO `users`(`name`,`rate`,`period`,`PV`,`initial_payment`,`total`) VALUES ('".mysqli_real_escape_string($link,$_POST['name'])."','".."','".."','".."','".."','".."')";
-            */
+       $query = "INSERT INTO `users`(`name`,`rate`,`period`,`PV`,`initial_payment`,`total`) VALUES ('".mysqli_real_escape_string($link,$_POST['name'])."',
+       '".mysqli_real_escape_string($link,$_POST['rate'])."',
+       '".mysqli_real_escape_string($link,$_POST['period'])."',
+       '".mysqli_real_escape_string($link,$_POST['PV'])."',
+       '".mysqli_real_escape_string($link,$_POST['initial_payment'])."',
+       '".mysqli_real_escape_string($link,$_POST['total'])."')";
+            
+        $result = mysqli_query($link, $query);
+        if($result){
+            echo "success";
+        }else{
+            echo "fail";
+        }
     }
 ?>
