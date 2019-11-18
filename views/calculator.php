8728187980c8554931ddc0cd7855c902573ed208
chris <chriscushway@gmail.com>
2019-11-18 11:27:37 +0200

    added styles to calculator

--- views/calculator.php
+++ views/calculator.php
@@ -1,10 +1,29 @@
 <div class="container">
-    <div>
-    
+    <div class="segment">
+        <div id="calculator">
+            <div><h2>Bond Calculator</h2></div>
+            <div>
+                <label>rate</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Period</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Down payment</label>
+                <input type="text">
+            </div>
+            <div>
+                <label>Amount</label>
+                <input type="text">
+            </div>
+            
+            <button id=calc-button>Calculate</button>
+        </div>
+        
     </div>
-    <div>
+    <div class="segment">
     
     </div>
 </div>
-</body>
-</html>
