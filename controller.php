24bcda6c6012599ca1cfa1a00d886865cd686426
chris <chriscushway@gmail.com>
2019-11-20 11:30:24 +0200

    added controller to output all saved calculations

--- controller.php
+++ controller.php
@@ -8,6 +8,21 @@
         exit();
         
     }
-
+    
+    function displayCalculations(){
+        
+        global $link;
+        $query = "SELECT * FROM `users`";
+        $result = mysqli_query($link, $query);
+        if(mysqli_num_rows($result) == 0){
+            echo "<h1>You have no saved calculations</h1>";
+        }else{
+            while($row = mysqli_fetch_assoc($result)){
+                echo "<div class='template'>".$row['name']."</div><div class='template'>".$row['name']."</div><div class='template'>".$row['name']."</div><div class='template'>".$row['name']."</div>";
+            }
+        }
+        
+        
+    }
 
 ?>
